1819d0c3cb3c1fc5c5efbceef4fe21a9ad21dbc1
Jessica Priebe <jessica.priebe@web.de>
2025-08-30 17:06:42 +0200

    Use two streamed matrices for OOC Matrix-Matrix Multiplication

--- src/main/java/org/apache/sysds/runtime/instructions/ooc/BinaryMatrixMatrixOOCInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/ooc/BinaryMatrixMatrixOOCInstruction.java
@@ -19,11 +19,9 @@
 
 package org.apache.sysds.runtime.instructions.ooc;
 
-import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 
 import org.apache.sysds.common.Opcodes;
-import org.apache.sysds.conf.ConfigurationManager;
 import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
@@ -53,7 +51,7 @@ public class BinaryMatrixMatrixOOCInstruction extends ComputationOOCInstruction 
 		InstructionUtils.checkNumFields(parts, 4);
 		String opcode = parts[0];
 		CPOperand in1 = new CPOperand(parts[1]); // the larget matrix (streamed)
-		CPOperand in2 = new CPOperand(parts[2]); // the small matrix (in-memory)
+		CPOperand in2 = new CPOperand(parts[2]); // the smaller matrix (streamed)
 		CPOperand out = new CPOperand(parts[3]);
 
 		AggregateOperator agg = new AggregateOperator(0, Plus.getPlusFnObject());
@@ -66,28 +64,18 @@ public class BinaryMatrixMatrixOOCInstruction extends ComputationOOCInstruction 
 	public void processInstruction( ExecutionContext ec ) {
 		// 1. Identify the inputs
 		MatrixObject in1 = ec.getMatrixObject(input1); // big matrix
-		MatrixBlock in2 = ec.getMatrixObject(input2)
-			.acquireReadAndRelease(); // in-memory matrix
+		MatrixObject in2 = ec.getMatrixObject(input2); // smaller matrix
 
-		// 2. Pre-partition the in-memory matrix into a hashmap
-		HashMap<Long, MatrixBlock> partitionedMatrix = new HashMap<>();
-		int blksize = in1.getDataCharacteristics().getBlocksize();
-		if (blksize < 0)
-			blksize = ConfigurationManager.getBlocksize();
-		long cols2 = (long) Math.ceil((double) in2.getNumColumns() / blksize);
-		for (int i=0; i < in2.getNumRows(); i+=blksize) {
-			for (int j=0; j < in2.getNumColumns(); j+=blksize) {
-				long key = (long) (i/blksize) * cols2 + (long) (j/blksize);
-				MatrixBlock slice = in2.slice(i, Math.min(i + blksize, in2.getNumRows())-1, j, Math.min(j + blksize, in2.getNumColumns())-1);
-				partitionedMatrix.put(key, slice);
-			}
-		}
+		long cols1 = in1.getDataCharacteristics().getNumColBlocks();
+		long cols2 = in2.getDataCharacteristics().getNumColBlocks();
 
-		// number of colBlocks for early block output
-		long emissionThreshold = in1.getDataCharacteristics().getNumColBlocks();
-		OOCMatrixBlockTracker aggTracker = new OOCMatrixBlockTracker(emissionThreshold);
+		// caches each block of the 2nd input only until that block is no longer needed
+		OOCMatrixBlockTracker in2Tracker = new OOCMatrixBlockTracker(in1.getDataCharacteristics().getNumRowBlocks());
+		// collects partial aggregation blocks until the block is ready to be emitted
+		OOCMatrixBlockTracker aggTracker = new OOCMatrixBlockTracker(cols1);
 
-		LocalTaskQueue<IndexedMatrixValue> qIn = in1.getStreamHandle();
+		LocalTaskQueue<IndexedMatrixValue> qIn1 = in1.getStreamHandle();
+		LocalTaskQueue<IndexedMatrixValue> qIn2 = in2.getStreamHandle();
 		LocalTaskQueue<IndexedMatrixValue> qOut = new LocalTaskQueue<>();
 		BinaryOperator plus = InstructionUtils.parseBinaryOperator(Opcodes.PLUS.toString());
 		ec.getMatrixObject(output).setStreamHandle(qOut);
@@ -96,31 +84,48 @@ public class BinaryMatrixMatrixOOCInstruction extends ComputationOOCInstruction 
 		try {
 			// Core logic: background thread
 			pool.submit(() -> {
-				IndexedMatrixValue tmp = null;
+				IndexedMatrixValue tmp1 = null;
+				IndexedMatrixValue tmp2 = null;
 				try {
-					while((tmp = qIn.dequeueTask()) != LocalTaskQueue.NO_MORE_TASKS) {
-						MatrixBlock block1 = (MatrixBlock) tmp.getValue();
-						long r1 = tmp.getIndexes().getRowIndex();
-						long c1 = tmp.getIndexes().getColumnIndex();
+					while((tmp1 = qIn1.dequeueTask()) != LocalTaskQueue.NO_MORE_TASKS) {
+						MatrixBlock block1 = (MatrixBlock) tmp1.getValue();
+						long r1 = tmp1.getIndexes().getRowIndex();
+						long c1 = tmp1.getIndexes().getColumnIndex();
 
 						// aggregation with all corresponding blocks
 						for (int j=0; j < cols2; j++) {
 							// r2 == c1
 							long key2 = (c1-1) * cols2 + j;
-							MatrixBlock block2 = partitionedMatrix.get(key2);
+							MatrixBlock block2 = in2Tracker.get(key2);
+							if(block2 == null) {
+								// corresponding block still in 2nd queue, dequeue until found
+								while((tmp2 = qIn2.dequeueTask()) != LocalTaskQueue.NO_MORE_TASKS) {
+									block2 = (MatrixBlock) tmp2.getValue();
+									long r2 = tmp2.getIndexes().getRowIndex();
+									long c2 = tmp2.getIndexes().getColumnIndex();
+									long key = (r2-1) * cols2 + (c2-1);
+									// store all dequeued blocks in cache
+									in2Tracker.putAndInitCount(key, block2);
+									// found corresponding block
+									if (key == key2) break;
+								}
+							}
 
 							// Now, call the operation with the correct, specific operator.
 							MatrixBlock partialResult = block1.aggregateBinaryOperations(block1, block2,
 								new MatrixBlock(), (AggregateBinaryOperator) _optr);
 
+							// early discard: remove block once it has been used for all of its corresponding aggregations
+							if(in2Tracker.incrementCount(key2)) in2Tracker.remove(key2);
+
 							// for single column block, no aggregation neeeded
-							if(emissionThreshold == 1) {
-								qOut.enqueueTask(new IndexedMatrixValue(tmp.getIndexes(), partialResult));
+							if(cols1 == 1) {
+								qOut.enqueueTask(new IndexedMatrixValue(tmp1.getIndexes(), partialResult));
 							}
 							else {
 								// index in the aggregated block: (r1, j+1)
 								long aggKey = (r1-1) * cols2 + j;
-								// aggregation
+								// accumulate partial aggregations
 								MatrixBlock currAgg = aggTracker.get(aggKey);
 								if(currAgg == null) {
 									aggTracker.putAndIncrementCount(aggKey, partialResult);
